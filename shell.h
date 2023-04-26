0b6d8425da6820bc0ff018eee6e28eb271018b9c
samuelfash7 <edwardsamuel@gmail.com>
2023-04-26 12:14:18 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -1,8 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 int interactive(info_t *info)
 int is_delim(char c, char *delim)
 int _isalpha(int c)
 int _atoi(char *s)
+int _myexit(info_t *info)
+int _mycd(info_t *info)
+
 #endif
 
