8630a52d27c038d6eb83b9c553235611bf9b7caf
samuelfash7 <edwardsamuel@gmail.com>
2023-04-26 15:28:59 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -1,12 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGS_LENGTH 10
 
 int interactive(info_t *info)
 int is_delim(char c, char *delim)
 int _isalpha(int c)
 int _atoi(char *s)
-int _myexit(info_t *info)
-int _mycd(info_t *info)
+int main(void)
+
 
 #endif
 
